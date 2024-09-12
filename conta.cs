b201c56a9cf760696fc6922f91618f749bc632c2
Nely Victória Oliveira da Silva <o.nely@escolar.ifrn.edu.br>
2024-09-12 16:24:10 -0300

    aula construtor

---
+++ conta.cs
@@ -0,0 +1,17 @@
+public class Conta
+{
+    // Atributos
+    public string nome;
+    public float cpf;
+    public int nºdaconta;
+    public int datanascimento;
+
+    // Construtor da classe(para inicializar os atributos)
+    public Conta(string nome, float cpf, int nºdaconta, int datanascimento)
+    {
+        nome = nomeDaPessoa;
+        cpf = cpfDaPessoa;
+        nºdaconta = numeroConta;
+        datanascimento = dataDapessoa;
+    }
+}
---
+++ principal.cs
@@ -0,0 +1,10 @@
+using System;
+class Principal
+{
+    static void Main(string[] args)
+    {
+        // Criando um objeto da classe conta
+        Conta minhaconta = new Conta ("fulana", 007.007.077.00, 188, 12092003);
+
+    }
+}
